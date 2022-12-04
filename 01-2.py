8c0fd993be097664d6c89aae031f45fdb03efa35
nogw <gabrielnogueiraoliveira@gmail.com>
2022-12-04 17:23:42 -0300

    day 1 part 2

---
+++ 01-2.py
@@ -0,0 +1,13 @@
+def readInput(filename: str) -> str:
+  f = open(filename, "r")
+  data = f.read()
+  f.close()
+  return data
+
+if __name__ == "__main__":
+  content = readInput("./input.txt").split("\n\n")
+  content = list(map(lambda e : e.split("\n"), content))
+  content = list(map(lambda e : list(map(int,e)) , content))
+  content = list(map(sum, content))
+  content.sort(reverse=True)
+  print(sum(content[0:3]))
